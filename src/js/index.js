91838eca457031c74e099c234f9e937ce995251a
Cathella <ncathie214@gmail.com>
2021-09-24 15:26:26 +0300

    style the todo

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,9 @@ import '../style.css';
 // import * as TODO from './app.js';
 import Task from './task.js';
 
-const task = new Task(1, "Eat lunch", true);
-console.log(task);
+const task = new Task(0, "Rest in lunch break", true);
+const task2 = new Task(1, "Submit pull request for review", false);
+const task3 = new Task(2, "Join session", false);
+
+
 
